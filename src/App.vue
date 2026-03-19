e47aa5a88b6cb7bc52275a2b3543b89b3d7d12c2
IgorMMichels <igor.marcon.michels@gmail.com>
2026-03-19 09:00:30 -0300

    Finalização Atividade To-Do List, Todas As Funções concluídas

--- src/App.vue
+++ src/App.vue
@@ -26,21 +26,34 @@ const tarefas = ref([
 const novaTarefa = ref('')
 
 function addTarefa(novaTarefa) {
-  const id = tarefas.value[tarefas.value.length - 1].id + 1
-  console.log(id)
-  tarefas.value.push({id: id, tarefa: novaTarefa, status: 'pendente'})
+  let id = 1
+  if (tarefas.value.length > 0) {
+    id = tarefas.value[tarefas.value.length - 1].id + 1
+  }
+  tarefas.value.push({ id: id, tarefa: novaTarefa, status: 'pendente' })
 }
 function editTarefa(id) {
-  const novoNome = prompt("Insira o novo nome da tarefa");
-  tarefas.value[id-1].tarefa = novoNome
+
+  const novoNome = prompt('Insira o novo nome da tarefa').trim()
+  if (novoNome.trim() != '') {
+    tarefas.value[id - 1].tarefa = novoNome
+  }
 }
 
 const tarefasFiltradas = computed(() => {
-  if (filtro.value.trim().value > 0) {
-    return tarefas.value.filter(item => item.tarefa.includes(filtro.value))
+  if (filtro.value.trim().length > 0) {
+    return tarefas.value.filter((item) => item.tarefa.includes(filtro.value))
   } else {
-    return tarefas.value;
+    return tarefas.value
   }
+})
+
+const concluidas = computed(() => {
+  return tarefas.value.filter(t => t.status === 'concluida').length
+})
+
+const pendentes = computed(() => {
+  return tarefas.value.filter(t => t.status === 'pendente').length
 })
 
 // Concluir Tarefa
@@ -49,22 +62,36 @@ const tarefasFiltradas = computed(() => {
 <template>
   <div class="container">
     <h1>Lista de Tarefas</h1>
-    <ul>
-      <li v-for="tarefa in tarefas" :key="tarefa" @click="tarefa.status = tarefa.status === 'concluida' ? 'pendente' : 'concluida'" :class="{concluida: tarefa.status == 'concluida'}">
-        {{ tarefa.tarefa }} <button @click="editTarefa(tarefa.id)">Edit</button>
-        <button @click="tarefas.splice(tarefas.indexOf(tarefa), 1)">Delete</button>
-      </li>
-    </ul>
     <input type="text" v-model="novaTarefa" />
-    <button @click="addTarefa(novaTarefa)">Adicionar</button>
-    <p>Concluídas: {{  }}</p>
-    <p>Pendentes: {{ }}</p>
+    <button @click="addTarefa(novaTarefa)">Add</button>
+    <ul>
+      <li
+        v-for="tarefa in tarefasFiltradas"
+        :key="tarefa"
+        @click="tarefa.status = tarefa.status === 'concluida' ? 'pendente' : 'concluida'"
+        :class="{ concluida: tarefa.status == 'concluida' }"
+      >
+        {{ tarefa.tarefa }} <button @click="editTarefa(tarefa.id)">Edit</button>
+        <button @click="tarefas.splice(tarefas.indexOf(tarefa), 1)">Del</button>
+      </li>
+      <p v-if="tarefasFiltradas.length == 0">Nenhum Resultado Encontrado</p>
+    </ul>
+    <input type="search" v-model="filtro" placeholder="Filtrar Tarefas...">
+    <p>Concluídas: {{  concluidas  }}</p>
+    <p>Pendentes: {{  pendentes  }}</p>
   </div>
 </template>
 
 <style scoped>
+button {
+  color: lime;
+  background: none;
+}
 li {
   cursor: pointer;
+}
+p {
+  margin: 0;
 }
 .concluida {
   text-decoration: line-through;
