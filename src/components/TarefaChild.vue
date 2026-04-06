5eb19e6f8f26bcd53dad9d63859e2d5c5fc92bcd
IgorMMichels <igor.marcon.michels@gmail.com>
2026-04-06 11:59:34 -0300

    Inicio Componentização

---
+++ src/components/TarefaChild.vue
@@ -0,0 +1,22 @@
+<script setup>
+  defineProps(['nome', 'basic', 'tipo'])
+</script>
+
+<template>
+  <button :class="[basic, tipo]">
+
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  color: lime;
+  background: none;
+}
+.tarefa {
+  cursor: pointer;
+}
+p {
+  margin: 0;
+}
+</style>
